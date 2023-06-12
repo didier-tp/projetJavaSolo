775127ff8c170ccfeb4b1430d60b449d19cf9c2b
Didier Defrance <didier@d-defrance.fr>
2023-06-12 14:30:51 +0200

    a

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -5,11 +5,8 @@ public class MyApp {
 	public static void main(String[] args) {
 		System.out.println("lundi 16 juin 2023 12h14");
 		System.out.println("bientot dejeuner");
-<<<<<<< HEAD
 		System.out.println("devA is the best");
-=======
-		System.out.println("devB is the best !!!");
->>>>>>> branch 'main' of https://github.com/didier-tp/projetJavaSolo.git
+		System.out.println("devB is the very best !!!");
 	}
 
 }
