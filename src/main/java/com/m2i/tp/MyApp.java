530caff57ff4928335feda9649d6e8294108aa69
Didier Defrance <didier@d-defrance.fr>
2023-06-12 13:53:02 +0200

    modif devA_the_best

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -5,6 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		System.out.println("lundi 16 juin 2023 12h14");
 		System.out.println("bientot dejeuner");
+		System.out.println("devA is the best");
 	}
 
 }
