d64125ad982f4dbb0f5c7a725e1ffa494bece0c9
Didier Defrance <didier@d-defrance.fr>
2023-06-12 13:50:31 +0200

    modif_devB

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -5,6 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		System.out.println("lundi 16 juin 2023 12h14");
 		System.out.println("bientot dejeuner");
+		System.out.println("devB is the best !!!");
 	}
 
 }
